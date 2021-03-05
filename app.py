cdebb13532618a32d4f96aa02218bd76dc899151
devendra-rgb <63838746+devendra-rgb@users.noreply.github.com>
2021-03-05 17:51:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+import pygal
+import json
+
+app=Flask(__name__)
+ma=pygal.maps.world.World()
+ma.title='Carona cases On 14/12/2020'
+with open('cleaned_data.json') as f:
+    dictionary=json.load(f)
+ma.add('Cases',dictionary)
+ma=ma.render_data_uri()
+@app.route('/')
+def hello():
+    return 'Hello World!!'
+
+
+@app.route('/line')
+def line_route():
+  
+  return render_template('charts.html',chart=ma)
+
+app.run(debug=True)
